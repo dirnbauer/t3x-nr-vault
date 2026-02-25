8a3fed33c6cf2e8b3455a1cc90723a4ccc67f325
Sebastian Mendel <info@sebastianmendel.de>
2026-02-26 00:40:25 +0100

    chore: add SPDX copyright and license headers to all PHP files
    Add per-file SPDX headers (Copyright Netresearch DTT GmbH, GPL-2.0-or-later)
    to all PHP source files for OpenSSF Best Practices Gold badge compliance.
    
    Signed-off-by: Sebastian Mendel <sebastian.mendel@netresearch.de>
    Signed-off-by: Sebastian Mendel <info@sebastianmendel.de>

--- Documentation/Developer/Adr/_PrefixedUsage.php
+++ Documentation/Developer/Adr/_PrefixedUsage.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * Copyright (c) 2025-2026 Netresearch DTT GmbH
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 // Pattern B: Prefixed reference usage
 // Extension setting value: vault:my_translation_api_key
 
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * Copyright (c) 2025-2026 Netresearch DTT GmbH
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Vault - Secure Secrets Management',
     'description' => 'Centralized, secure storage for API keys, credentials, and other secrets with envelope encryption, access control, audit logging, and a secure HTTP client.',
